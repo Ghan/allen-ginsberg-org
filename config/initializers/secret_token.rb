861377f28a3c860f95d4cfcebdf985424589dfe0
Ghan Patel <ghanpatel@gmail.com>
2013-07-30 18:55:28 -0400

    launching site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllenGinsbergOrg::Application.config.secret_token = '67e75fa90399037f0ec1e7c86b8b9c6192dd2bd9fe970b50a052337ab9cf6486eb78a3c2c0ba715c438290e82588e14c13458ab34a82b8d873184fe281eb03b4'
