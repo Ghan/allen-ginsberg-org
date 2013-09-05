11502dcc6b27ff07fd6876b4c4f0951606b06c39
Ghan Patel <ghanpatel@gmail.com>
2013-09-05 12:26:32 -0400

    trying another fix

--- app/controllers/locomotive/api/content_entries_controller.rb
+++ app/controllers/locomotive/api/content_entries_controller.rb
@@ -379,6 +379,29 @@ module Locomotive
         end
         respond_with @content_entry, status: @content_entry ? :ok : :not_found
       end
+
+      def create
+        @content_entry.from_presenter(params[:content_entry] || params[:entry])
+        @content_entry.save
+        respond_with @content_entry, location: main_app.locomotive_api_content_entries_url(@content_type.slug)
+      end
+
+      def update
+        @content_entry.from_presenter(params[:content_entry] || params[:entry])
+        @content_entry.save
+        respond_with @content_entry, location: main_app.locomotive_api_content_entries_url(@content_type.slug)
+      end
+
+      def destroy
+        @content_entry.destroy
+        respond_with @content_entry, location: main_app.locomotive_api_content_entries_url(@content_type.slug)
+      end
+
+      protected
+
+      def content_type
+        @content_type ||= current_site.content_types.where(slug: params[:slug]).first
+      end
     end
   end
 end
