e78438b23b57a46d6a0df67ad4ea2d375014a4fe
Ghan Patel <ghanpatel@gmail.com>
2013-09-05 17:03:06 -0400

    making formatting changes, added fonts and colors

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -19,7 +19,6 @@ end
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
 
-app.cache_duration = 3600*24*365*3
 ## configure it ##
 
 Dragonfly[:images].configure do |c|
@@ -33,18 +32,6 @@ Dragonfly[:images].configure do |c|
 
   c.url_format = '/images/dynamic/:job/:basename.:format'
 
-  # # Override the .url method...
-  # c.define_url do |app, job, opts|
-  #   thumb = Thumb.find_by_job(job.serialize)
-  #   # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
-  #   if thumb
-  #     app.datastore.url_for(thumb.uid)
-  #   # ...otherwise give the local Dragonfly server url
-  #   else
-  #     app.server.url_for(job)
-  #   end
-  # end
-
   # # Before serving from the local Dragonfly server...
   # c.server.before_serve do |job, env|
   #   # ...store the thumbnail in the datastore...
@@ -55,6 +42,18 @@ Dragonfly[:images].configure do |c|
   #     :uid => uid,
   #     :job => job.serialize     # 'BAhbBls...' - holds all the job info
   #   )                           # e.g. fetch 'some_uid' then resize to '40x40'
+  # end
+  
+  # # Override the .url method...
+  # c.define_url do |app, job, opts|
+  #   thumb = Thumb.find_by_job(job.serialize)
+  #   # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
+  #   if thumb
+  #     app.datastore.url_for(thumb.uid)
+  #   # ...otherwise give the local Dragonfly server url
+  #   else
+  #     app.server.url_for(job)
+  #   end
   # end
 
 end
