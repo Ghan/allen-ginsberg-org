dba86552c58d0a2da5199b060e5cd9a9960bbf2f
Ghan Patel <ghanpatel@gmail.com>
2013-09-05 12:15:51 -0400

    trying another fix

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -6,7 +6,7 @@ app = Dragonfly[:images]
 app.configure_with(:rails)
 app.configure_with(:imagemagick)
 app.configure_with(:heroku, 'allenginsberg') if Rails.env.production?
-app.define_macro(ActiveRecord::Base, :image_accessor)
+# app.define_macro(ActiveRecord::Base, :image_accessor)
 
 ## configure it ##
 
