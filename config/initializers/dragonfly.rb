5831fade933c07ab6bb95187300fc623bdef7eca
Ghan Patel <ghanpatel@gmail.com>
2013-09-10 12:00:16 -0400

    fixing s3 bucket

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -7,37 +7,37 @@ app = Dragonfly[:images]
 app.configure_with(:rails)
 app.configure_with(:imagemagick)
 
-if Rails.env.production?
+# if Rails.env.production?
+if true
   app.configure do |c|
     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
       :bucket_name => 'allenginsberg',
-      :access_key_id => 'AKIAIGVAQZAGNRCVPQJA',
-      :secret_access_key => '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',
-      :region => 'us-west-2'
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
     )
     # trial
     # c.url_host = 'http://allen-ginsberg-org.herokuapp.com'
       # First we configure our before_serve method,
       # Before serving, the first time it is requested stores the thumbnail in the datastore
-    c.server.before_serve do |job, env|
-      uid = job.store
-      # Keep track of its uid
-      # Holds all the job info, e.g fetch 'image_uid' then resize to '40x40'
-      Thumb.create!( :uid => uid, :job => job.serialize )
-    end
+    # c.server.before_serve do |job, env|
+    #   uid = job.store
+    #   # Keep track of its uid
+    #   # Holds all the job info, e.g fetch 'image_uid' then resize to '40x40'
+    #   Thumb.create!( :uid => uid, :job => job.serialize )
+    # end
 
-    # Next we define the url for our processed images, overriding the default .url method...
-    c.define_url do |app, job, opts|
-      thumb = Thumb.find_by_job(job.serialize)
-      # If (the job fetch 'image_uid' then resize to '40x40') has been stored already..
-      # then serve the url from the datastore filesystem, s3, etc
-      if thumb
-        app.datastore.url_for(thumb.uid)
-      else
-      # ...otherwise if the job hasn't been stored, serve it from the Dragonfly server as usual
-        app.server.url_for(job)
-      end
-    end
+    # # Next we define the url for our processed images, overriding the default .url method...
+    # c.define_url do |app, job, opts|
+    #   thumb = Thumb.find_by_job(job.serialize)
+    #   # If (the job fetch 'image_uid' then resize to '40x40') has been stored already..
+    #   # then serve the url from the datastore filesystem, s3, etc
+    #   if thumb
+    #     app.datastore.url_for(thumb.uid)
+    #   else
+    #   # ...otherwise if the job hasn't been stored, serve it from the Dragonfly server as usual
+    #     app.server.url_for(job)
+    #   end
+    # end
     # end trial
   end
 
@@ -54,6 +54,7 @@ if Rails.env.production?
 end
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
+
 
 ## configure it ##
 
