7907035354740fd266913751bc5d163dc179325f
Ghan Patel <ghanpatel@gmail.com>
2013-09-10 09:16:04 -0400

    added dragonfly logic

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -15,6 +15,18 @@ if Rails.env.production?
       :secret_access_key => ENV['S3_SECRET']
     )
   end
+
+  # Alt method
+  app.datastore = Dragonfly::DataStorage::S3DataStore.new
+  app.datastore.configure do |c|
+    c.bucket_name = 'allenginsberg'
+    c.access_key_id = ENV['S3_KEY']
+    c.secret_access_key = ENV['S3_SECRET']
+    c.region = 'us-west-2'                        # defaults to 'us-east-1'
+    # c.storage_headers = {'some' => 'thing'}       # defaults to {'x-amz-acl' => 'public-read'}
+    # c.url_scheme = 'https'                        # defaults to 'http'  
+  end
+
 end
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
@@ -32,28 +44,28 @@ Dragonfly[:images].configure do |c|
 
   c.url_format = '/images/dynamic/:job/:basename.:format'
 
-  # # Before serving from the local Dragonfly server...
-  # c.server.before_serve do |job, env|
-  #   # ...store the thumbnail in the datastore...
-  #   uid = job.store
+  # Override the .url method...
+  c.define_url do |app, job, opts|
+    thumb = Thumb.find_by_job(job.serialize)
+    # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
+    if thumb
+      app.datastore.url_for(thumb.uid)
+    # ...otherwise give the local Dragonfly server url
+    else
+      app.server.url_for(job)
+    end
+  end
 
-  #   # ...keep track of its uid so next time we can serve directly from the datastore
-  #   Thumb.create!(
-  #     :uid => uid,
-  #     :job => job.serialize     # 'BAhbBls...' - holds all the job info
-  #   )                           # e.g. fetch 'some_uid' then resize to '40x40'
-  # end
+  # Before serving from the local Dragonfly server...
+  c.server.before_serve do |job, env|
+    # ...store the thumbnail in the datastore...
+    uid = job.store
+
+    # ...keep track of its uid so next time we can serve directly from the datastore
+    Thumb.create!(
+      :uid => uid,
+      :job => job.serialize     # 'BAhbBls...' - holds all the job info
+    )                           # e.g. fetch 'some_uid' then resize to '40x40'
+  end
   
-  # # Override the .url method...
-  # c.define_url do |app, job, opts|
-  #   thumb = Thumb.find_by_job(job.serialize)
-  #   # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
-  #   if thumb
-  #     app.datastore.url_for(thumb.uid)
-  #   # ...otherwise give the local Dragonfly server url
-  #   else
-  #     app.server.url_for(job)
-  #   end
-  # end
-
 end
