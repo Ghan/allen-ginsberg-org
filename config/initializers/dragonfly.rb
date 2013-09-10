4fad05af33c316ba58fb1dae943ea5a2d0c7736c
Ghan Patel <ghanpatel@gmail.com>
2013-09-10 10:26:29 -0400

    trial fix for dragonfly

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -11,8 +11,8 @@ if Rails.env.production?
   app.configure do |c|
     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
       :bucket_name => 'allenginsberg',
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
+      :access_key_id => 'AKIAIGVAQZAGNRCVPQJA',
+      :secret_access_key => '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'
     )
     # trial
     # c.url_host = 'http://allen-ginsberg-org.herokuapp.com'
@@ -66,29 +66,5 @@ Dragonfly[:images].configure do |c|
   c.allow_fetch_file = true
 
   c.url_format = '/images/dynamic/:job/:basename.:format'
-
-  # # Override the .url method...
-  # c.define_url do |app, job, opts|
-  #   thumb = Thumb.find_by_job(job.serialize)
-  #   # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
-  #   if thumb
-  #     app.datastore.url_for(thumb.uid)
-  #   # ...otherwise give the local Dragonfly server url
-  #   else
-  #     app.server.url_for(job)
-  #   end
-  # end
-
-  # # Before serving from the local Dragonfly server...
-  # c.server.before_serve do |job, env|
-  #   # ...store the thumbnail in the datastore...
-  #   uid = job.store
-
-  #   # ...keep track of its uid so next time we can serve directly from the datastore
-  #   Thumb.create!(
-  #     :uid => uid,
-  #     :job => job.serialize     # 'BAhbBls...' - holds all the job info
-  #   )                           # e.g. fetch 'some_uid' then resize to '40x40'
-  # end
 
 end
