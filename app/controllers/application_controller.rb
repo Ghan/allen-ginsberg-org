3828027359823bb35ec1e5ac3bd28a79ee719d84
Ghan Patel <ghanpatel@gmail.com>
2014-02-15 15:18:38 -0500

    fixing domain 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   	@appPath = "http://localhost:8080/"
     @S3imgURL = "https://allenginsberg.s3.amazonaws.com"
   	if Rails.env.production?
-	  	@appPath = "http://www.allenginsberg.com/"
+	  	@appPath = "http://www.allenginsberg.org/"
       @S3imgURL = ""
 	  end
 	end
