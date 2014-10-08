ce6aa263d2b98e01d78117c04bcee3b62c648ab5
Ghan Patel <ghanpatel@gmail.com>
2014-10-08 00:51:08 -0400

    diff unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+before_fork do |server, worker|
+  # other settings
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  # other settings
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool']            =   ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
