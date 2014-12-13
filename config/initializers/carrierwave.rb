8db1e7d48151b2ae1d964484b88e45b953de7703
Ghan Patel <ghanpatel@gmail.com>
2014-12-13 16:22:42 -0500

    using env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,8 +22,8 @@ CarrierWave.configure do |config|
     config.storage          = :fog
     config.fog_credentials  = {
       :provider                 => 'AWS',
-      :aws_access_key_id        => "AKIAJXQ2XPWNBOBGCL3A",
-      :aws_secret_access_key    => "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",
+      :aws_access_key_id        => ENV['S3_KEY'],
+      :aws_secret_access_key    => ENV['S3_SECRET'],
       :region                   => "us-east-1"
     }
     config.fog_directory    = "allenginsberg"
